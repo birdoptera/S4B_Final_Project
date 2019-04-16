e8869d71e02ff3660b85db7991f7ae6add624004
Iwo Gross <iwo@iwo-gross-s-macbook.local>
2019-04-16 00:27:57 -0500

    Housekeeping and updating.
    Added a prompt to hard-code the genus-species nomenclature.
    Update idigbio and gbif search commands to have limits of 5000 occurrences (especially important for gbif, which is very slow)
    Streamlined the combination of both matrices.

--- S4B_Download_and_rasterize_species_data.R
+++ S4B_Download_and_rasterize_species_data.R
@@ -20,15 +20,19 @@ library(ridigbio)
 ## the variables and imput multiple species etc. If you want to change the code to that, please go ahead. -Gwendolyn
 ## Not sure where everyone has been working, but I am adding code below that downloads and combines long/lat data from idigbio (with Gwendolyn's code) and GBIF -Iwo
 
-#I chose Myzus persicae (pea aphid) because I know it's a common pest and so would have good collection data
-speciesdata <- idig_search_records(rq = list(geopoint=list(type="exists"), genus = "myzus", "data.dwc:specificEpithet" = "persicae"),
+#Prompt and hard-code [genus] [species] names
+genus <- readline(prompt= "Enter a genus name: ")
+species <- readline(prompt= "Enter a species name: ")
 
-                                   fields = c("geopoint"))
+#I chose Myzus persicae (pea aphid) because I know it's a common pest and so would have good collection data 
+## I added hard-coded nomenclature variables and a limit parameter. The latter probably isn't necessary since idigbio is a smaller database, but it might save time for larger species datasets -Iwo
+datum_idigbio <- idig_search_records(rq = list(geopoint=list(type="exists"), genus = sprintf("%s", genus)), "data.dwc:specificEpithet" = sprintf("%s", species), fields = c("geopoint"), limit=5000)
+
 #check your data
-head(speciesdata)
+head(datum_idigbio)
 
 #save it so you don't have to download it all over again
-write.csv(speciesdata, "speciesdata.csv")
+write.csv(datum_idigbio, "datum_idigbio.csv")
 
 ## START of Iwo's edits
 
@@ -39,27 +43,25 @@ library(rgbif)
 #search for all GBIF records of a given "[Genus] [species]"
 ## I included an exaggerated limit of 5000 bc the default is 500 (too low), and the "per request maximum" is 300 -Iwo
 ## I don't understand that part of the help page on CRAN, but "5000" does the trick so I'm sticking with it -Iwo
-datum <- occ_search(scientificName = "Myzus persicae", return = "data",limit="5000")
-
-#format columns to match Ridigbio data
-datum_gbif <- datum[,4:3]
+## I figured it out. This is a better command: hard-coded variables, returns up to 5000 records with long/lat data only, not necessary to remove NA cases later -Iwo
+datum_gbif <- occ_search(scientificName = sprintf("%s %s", genus, species), country= "us", return = "data",limit="5000", hasCoordinate=TRUE, fields=c('decimalLongitude','decimalLatitude'))
 
 #check your data
 head(datum_gbif)
 
 #rename gbif columns according to idigbio
 ## Is this necessary? Not sure -Iwo
-names(datum_gbif) <- names(speciesdata)
+names(datum_gbif) <- names(datum_idigbio)
 
 #combine idigbio and gbif occurrence records
-datum_combined <- rbind(speciesdata, datum_gbif)
+speciesdata <- rbind(datum_idigbio, datum_gbif)
 
 #Remove cases with NA lat/long data
-## Several NAs made it through gbif, probably because my search wasn't sophisticated enough- Iwo
-datum_combined <- datum_combined[complete.cases(datum_combined, ]
+## This is probably no longer necessary for gbif, but it might catch NAs from idigbio -Iwo
+speciesdata <- speciesdata[complete.cases(speciesdata), ]
 
 #save it so you don't have to download it all over again
-write.csv(datum_combined, "datum_combined.csv")
+write.csv(speciesdata, "speciesdata.csv")
 
 ## END of Iwo's edits 
 
